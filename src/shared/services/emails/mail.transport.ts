cd7d3264d4dc2883b763efc403569a54e8e3a391
LPastine <lucianopastine.tech@gmail.com>
2023-10-02 20:00:05 -0300

    adjust mail service

--- src/shared/services/emails/mail.transport.ts
+++ src/shared/services/emails/mail.transport.ts
@@ -28,8 +28,8 @@ class MailTransport {
   private async developmentEmailSender(receiverEmail: string, subject: string, body: string): Promise<void> {
     const transporter: Mail = nodemailer.createTransport({
       host: 'smtp.ethereal.email',
-      port: 465,
-      secure: true,
+      port: 587,
+      secure: false,
       auth: {
         user: config.SENDER_EMAIL,
         pass: config.SENDER_EMAIL_PASSWORD
