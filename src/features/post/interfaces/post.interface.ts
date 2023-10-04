b292de8ca80a7bac049a205c2158cca66e3e4bb5
LPastine <lucianopastine.tech@gmail.com>
2023-10-04 20:16:27 -0300

    post queue and worker

--- src/features/post/interfaces/post.interface.ts
+++ src/features/post/interfaces/post.interface.ts
@@ -1,7 +1,7 @@
 import { ObjectId } from 'mongodb';
 import mongoose, { Document } from 'mongoose';
 
-// TODO - Move this to reactions interface later
+// TO DO - Move this to reactions interface later
 interface IReactions {
   like: number;
   love: number;
