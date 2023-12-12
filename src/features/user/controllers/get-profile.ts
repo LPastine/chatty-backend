31bda37bd0f97e705a1409c13911f030487f5b5c
LPastine <lucianopastine.tech@gmail.com>
2023-12-12 19:29:31 -0300

    get user profile and posts

--- src/features/user/controllers/get-profile.ts
+++ src/features/user/controllers/get-profile.ts
@@ -8,6 +8,9 @@ import { userService } from '@service/db/user.service';
 import { IFollowerData } from '@follower/interfaces/follower.interface';
 import { followerService } from '@service/db/follower.service';
 import mongoose from 'mongoose';
+import { Helpers } from '@global/helpers/helpers';
+import { IPostDocument } from '@post/interfaces/post.interface';
+import { postService } from '@service/db/post.service';
 
 interface IUserAll {
   newSkip: number;
@@ -50,6 +53,20 @@ export class Get {
     const cachedUser: IUserDocument = (await userCache.getUserFromCache(userId)) as IUserDocument;
     const existingUser: IUserDocument = cachedUser ? cachedUser : await userService.getUserById(`${req.currentUser!.userId}`);
     res.status(HTTP_STATUS.OK).json({ message: 'Get user profile by id', user: existingUser });
+  }
+
+  public async profileAndPosts(req: Request, res: Response): Promise<void> {
+    const { userId, username, uId } = req.params;
+    const userName: string = Helpers.firstLetterUppercase(username);
+    const cachedUser: IUserDocument = (await userCache.getUserFromCache(userId)) as IUserDocument;
+    const cachedUserPost: IPostDocument[] = await postCache.getUserPostsFromCache('post', parseInt(uId, 10));
+
+    const existingUser: IUserDocument = cachedUser ? cachedUser : await userService.getUserById(`${req.currentUser!.userId}`);
+    const userPosts: IPostDocument[] = cachedUserPost.length
+      ? cachedUserPost
+      : await postService.getPosts({ username: userName }, 0, 100, { createdAt: -1 });
+
+    res.status(HTTP_STATUS.OK).json({ message: 'Get user profile and posts', user: existingUser, posts: userPosts });
   }
 
   private async allUsers({ newSkip, limit, skip, userId }: IUserAll): Promise<IAllUsers> {
