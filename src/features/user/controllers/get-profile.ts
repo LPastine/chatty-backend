070f59668579d3db449407895022b3ae2d046463
LPastine <lucianopastine.tech@gmail.com>
2023-12-12 18:10:10 -0300

    fix user followers bug

--- src/features/user/controllers/get-profile.ts
+++ src/features/user/controllers/get-profile.ts
@@ -61,7 +61,7 @@ export class Get {
 
   private async followers(userId: string): Promise<IFollowerData[]> {
     const cachedFollowers: IFollowerData[] = await followerCache.getFollowersFromCache(`followers:${userId}`);
-    const result = cachedFollowers.length ? cachedFollowers : await followerService.getFolloweeData(new mongoose.Types.ObjectId(userId));
+    const result = cachedFollowers.length ? cachedFollowers : await followerService.getFollowerData(new mongoose.Types.ObjectId(userId));
     return result;
   }
 }
