dccf1a38dd0905d5ffe470f6d939199bdbaa4c53
LPastine <lucianopastine.tech@gmail.com>
2023-12-12 21:31:42 -0300

    change password

--- src/features/user/routes/userRoutes.ts
+++ src/features/user/routes/userRoutes.ts
@@ -1,4 +1,5 @@
 import { authMiddleware } from '@global/helpers/auth-middleware';
+import { Update } from '@user/controllers/change-password';
 import { Get } from '@user/controllers/get-profile';
 import { Search } from '@user/controllers/search-user';
 import express, { Router } from 'express';
@@ -17,6 +18,8 @@ class UserRoutes {
     this.router.get('/user/profile/posts/:username/:userId/:uId', authMiddleware.checkAuthentication, Get.prototype.profileAndPosts);
     this.router.get('/user/profile/user/suggestions', authMiddleware.checkAuthentication, Get.prototype.randomUserSuggestions);
     this.router.get('/user/profile/search/:query', authMiddleware.checkAuthentication, Search.prototype.user);
+
+    this.router.put('/user/profile/change-password', authMiddleware.checkAuthentication, Update.prototype.password);
 
     return this.router;
   }
